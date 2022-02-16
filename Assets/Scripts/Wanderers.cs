6be6647d2b395155c45d7df1cde1f466cd1fb13e
lucpng <lucpinguet91@gmail.com>
2022-02-16 01:02:44 +0100

    litte stuff missing in the last commit

--- Assets/Scripts/Wanderers.cs
+++ Assets/Scripts/Wanderers.cs
@@ -31,7 +31,6 @@ public class Wanderers : MonoBehaviour
     bool SetDestinationRandom()
     {
         Vector3 newDestination = new Vector3(Random.Range(0.0f, GameManager.terrainSize.x), 0.0f, Random.Range(0.0f, GameManager.terrainSize.y));
-        
         NavMeshHit hit;
         NavMesh.SamplePosition(newDestination, out hit, float.PositiveInfinity, NavMesh.AllAreas);
         NavMesh.CalculatePath(this.transform.position, hit.position, NavMesh.AllAreas, path);
